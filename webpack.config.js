5b26e109af4991eb4d08dac6b625ea388feb2076
Aleksei <lexa24032@gmail.com>
2022-07-21 17:29:06 +0300

    setting configuration for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,21 @@
 const path = require('path');
+const { webpack } = require('webpack');
+
+// ==============================================
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'production'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+      },
+    ],
+  },
+};

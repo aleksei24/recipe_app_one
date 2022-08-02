aef21bfe6dff9d0c041bcc185242f78ee48c2fb6
Aleksei <lexa24032@gmail.com>
2022-08-02 17:16:12 +0300

    will try scss

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 import './script/view';
 import './script/getData';
-import './script/style';
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,5 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const postcssJs = require('postcss-js');
-const autoprefixer = require('autoprefixer');
-
-const prefixer = postcssJs.sync([autoprefixer]);
-const style = prefixer({
-  userSelect: 'none',
-});
 
 // ==============================================
 
@@ -27,27 +20,6 @@ module.exports = {
     rules: [
       {
         test: /\.js$/i,
-      },
-      {
-        test: '/.style.js$/',
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 2,
-            },
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                parser: 'postcss-js',
-              },
-              execute: true,
-            },
-          },
-        ],
       },
     ],
   },
